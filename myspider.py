a66099cecb9be9d4b6196b701b73168baa5129ee
Youshin Kim <kim1415@purdue.edu>
2018-09-23 12:31:15 -0400

    scrape headlines recursively

--- myspider.py
+++ myspider.py
@@ -8,13 +8,16 @@ class BlogSpider(scrapy.Spider):
         lines = response.css('#leftContentArea').extract()[0]
         Articles = response.css('div.remainingArticles').extract()
         headlines = response.css('h2.headline a::text').extract()
-
+        names = []
         #print(Articles)
         #print(lines)
         for headline in headlines:
-            print(headline)
-        
-        # next_page = response.css('.tekpagination a::attr(href)').extract_first()
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)
+            names.append(headline)
+        next_page = response.css('.tekpagination a::attr(href)').extract()
+        if len(next_page) == 2:
+            next_page = next_page[1]
+        else:
+            next_page = next_page[0]
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
